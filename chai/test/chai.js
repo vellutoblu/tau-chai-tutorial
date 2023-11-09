4b8f6fa5f75131014a3d147a3c320dc67d15358e
Matias <matiasvaca993@gmail.com>
2023-11-08 23:54:58 -0300

    o

--- chai/test/chai.js
+++ chai/test/chai.js
@@ -39,35 +39,34 @@ x = new myObj(), y= new myObj();
 
 //deep.equal or deep.equals
 expect(x).to.be.deep.equals(y, 'x and y are not equal');
-/*
-x.should.to.be.deep.equals(y, 'x and y are not equal');
+
+//x.should.to.be.deep.equals(y, 'x and y are not equal');
 //assert
-assert.deepEqual(x,y, 'x and y are not equal');
+//assert.deepEqual(x,y, 'x and y are not equal');
 
 //chaining expressions
 // x is an object; x and y are equal
 // keywords- and, with, but, ...
 expect(x).to.be.an('object').and.to.be.deep.equals(y);
-x.should.to.be.an('object').and.to.be.deep.equals(y);
+//x.should.to.be.an('object').and.to.be.deep.equals(y);
 
 //arrays
-let numbers = [1,2,3,0];
-expect(numbers).to.be.an('array').that.includes(3);
+//let numbers = [1,2,3,0];
+//expect(numbers).to.be.an('array').that.includes(3);
 //should()
-(numbers).should.be.an('array').that.includes(3);
-assert.isArray(numbers, 'numbers is not an array');
+//(numbers).should.be.an('array').that.includes(3);
+//assert.isArray(numbers, 'numbers is not an array');
 
 //should extras
 //should -> Object.Prototype - a.should
-function writeToAFile(error){
+//function writeToAFile(error){
     //should extras
 
     // normal scenario
     // error.should.not.exist();
 
     //Given that error is undefined
-    should.not.exist(error);
-}
+ //   should.not.exist(error);
+//}
 
-writeToAFile(undefined);
-*/
+//writeToAFile(undefined);
