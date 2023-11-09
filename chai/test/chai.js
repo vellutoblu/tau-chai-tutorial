39ff2d6304ff41b035e09fed0a5c1a8427daad7f
Matias <matiasvaca993@gmail.com>
2023-11-08 23:47:21 -0300

    object equals

--- chai/test/chai.js
+++ chai/test/chai.js
@@ -34,7 +34,7 @@ function myObj(){
 
 x = new myObj(), y= new myObj();
 expect(x).to.be.an('object');
-// expect(x).to.be.equals(y, 'x and y are not equal');
+expect(x).to.be.equals(y, 'x and y are not equal');
 
 /*
 //deep.equal or deep.equals
